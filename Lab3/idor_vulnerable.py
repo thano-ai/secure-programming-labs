8d4422ae1f91f8c2f39f8f95c0297530120398b5
Thana Abdulhalim <thanaaashwal3112000@gmail.com>
2026-07-09 17:05:34 +0300

    enhance IDOR Lab attack

--- Lab3/idor_vulnerable.py
+++ Lab3/idor_vulnerable.py
@@ -4,35 +4,22 @@ app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
 
 # Mock database
-## user_docs = users[username][documents]
 users = {
     'user1': {'password': '1234', 'documents': [1, 3]},
     'user2': {'password': 'pass2', 'documents': [2]},
-    'ali': {'password': 'pass', 'documents': [4,5]}
-}
 
-### users[user1][document]
+}
 
 documents = {
     1: {"owner": "user1", "content": "this is content"},
     2: {"owner": "user2", "content": "Secret document 2"},
-    3: {"owner": "user1", "content": "Secret document 3"},
-    4: {"owner": 'ali', "content": "Document Content"},
-    5: {"owner": 'ali', "content": "Document Content"},
+    3: {"owner": "user1", "content": "Secret document 3"}
 }
-
 
 @app.route('/')
 def home():
-    # if 'username' in session:
-    #     user_docs = users[session['username']]['documents'] ## user_docs = [6,7,8]
-    #     return render_template('dashboard.html',
-    #                            username=session['username'],
-    #                            document_ids=user_docs)
-    # return render_template('login.html')
-
     if 'username' in session:
-        doc_ids = users[session['username']]['documents'] ## [1,3]
+        doc_ids = users[session['username']]['documents']
 
         return render_template('dashboard.html', username=session['username'], document_ids=doc_ids)
     return render_template('login.html')
@@ -63,16 +50,8 @@ def view_document(doc_id):
     if doc_id not in documents:
         return "Document not found", 404
 
-    if session['username'] != documents[doc_id]['owner']:
-        return "Unauthorized -- ", 403
-
-
     document = documents[doc_id]
     return render_template('document.html', username=session['username'], document=document, doc_id=doc_id)
-
-
-
-
 
 
 
